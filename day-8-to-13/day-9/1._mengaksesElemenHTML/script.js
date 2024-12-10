4cc342db93ee3b0a9a5bb225b2b0239e2e5b3496
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2024-12-10 18:56:21 +0700

    ✅ Done Day 9

---
+++ day-8-to-13/day-9/1._mengaksesElemenHTML/script.js
@@ -0,0 +1,26 @@
+// ! Dom Manipulation
+
+// ! getElementById()
+
+const title = document.getElementById('main-heading')
+console.log("🚀 ~ title:", title)
+
+// ! getElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items')
+console.log("🚀 ~ listItem:", listItem)
+
+// ! getElementByTagName()
+
+const listItemTag = document.getElementsByTagName('li')
+console.log("🚀 ~ listItemTag:", listItemTag)
+
+// ! querySelector()
+
+const container = document.querySelector('div')
+console.log("🚀 ~ container:", container)
+
+// ! querySelectorAll()
+
+const containerAll = document.querySelectorAll('div')
+console.log("🚀 ~ containerAll:", containerAll)
---
+++ day-8-to-13/day-9/Quiz-Project/buttonAlert/script.js
@@ -0,0 +1,6 @@
+
+// Mengakses Element
+
+const getAlert = document.getElementById('btnAlert')
+const tampilAlert = () => alert('Tombol Di Klik');
+getAlert.addEventListener('click', tampilAlert)
